77e3c6850e18d59468c6628bfd794eb24cc250bb
Gabrielle O'Brien <gobrien088@gmail.com>
2020-01-07 15:37:36 -0800

    basic styling for footer

---
+++ client/src/web/components/Footer/BottomCard/BottomCard.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {Card, CardBody, CardTitle, CardSubtitle, CardText} from 'reactstrap';
+import "./BottomCard.scss";
+import ReadMore from '../../ReadMore/ReadMore';
+import axios from 'axios';
+
+export default class BottomCard extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      articleTitle:"Article Title",
+      previewText:
+      "Lorem ipsum dolor sit amet, nec timeam docendi placerat ea, amet sententiae theophrastus ex ... Usu ad etiam clita nominati, pri putant iracundia ea. Te mei erat dicat aperiam, cum an euismod forensibus, nec an odio elit intellegebat. ... Et usu fierent detracto perfecto, ea odio volutpat splendide pri, solum forensibus est ei.",
+      author:"Author Name",
+      date:"MM/DD/YY"
+    }
+  }
+
+  getData(){
+    axios.get('http://localhost:3000/')
+    .then(
+      (response) =>
+      {
+        this.setState({
+          articleTitle: this.response.icon,
+          previewText: this.response.previewText,
+          author: this.response.author,
+          date: this.response.date,
+        },
+      (error) =>
+      {
+        console.log(error);
+      });
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <Card>
+          <CardBody>
+            <CardTitle>{this.state.articleTitle}</CardTitle>
+            <CardSubtitle>{this.state.date}</CardSubtitle>
+            <CardText>{this.state.previewText}</CardText>
+            <ReadMore />
+          </CardBody>
+        </Card>
+      </div>
+    )
+  }
+}
--- client/src/web/components/Footer/Footer.js
+++ client/src/web/components/Footer/Footer.js
@@ -1,22 +1,48 @@
-import React, {Fragment} from 'react';
-import {Row, Col} from 'reactstrap';
+import React from 'react';
+import {Row, Col, Button} from 'reactstrap';
+import BottomCard from './BottomCard/BottomCard';
 import "./Footer.scss";
 
 
 export default function Footer(){
   return(
-    <Fragment>
-    <Row className="containfoot">
-      <p className="leftheader">Be heard.</p>
-      <Col md="3">
-      </Col>
-      <Col md="3">
-      </Col>
-      <Col md="3">
-      </Col>
-      <Col md="3">
-      </Col>
-    </Row>
-    </Fragment>
+    <div className="containfoot">
+      <Row >
+        <Col md="3">
+          <p className="leftheader">Be heard.</p>
+          <Button outline color="primary" className="post"> Start Talking </Button>
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+      </Row>
+      <Row className="below">
+        <Col md="3">
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+        <Col md="3">
+          <BottomCard />
+        </Col>
+      </Row>
+      <Row className="below">
+        <Col md="3">
+        </Col>
+        <Col md="9">
+          <p className="contact"> ____________________________________________________
+          <br/>Contact Us</p>
+        </Col>
+      </Row>
+    </div>
   );
 }
