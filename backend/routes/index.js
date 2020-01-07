c11993fc71a9238effe3fb9b07522e34584625f9
SwampApe <rothvann2000@gmail.com>
2020-01-07 12:00:28 -0800

    Moving routing files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,4 @@
-var express = require('express');
-var router = express.Router();
+var api = require('./api');
+var router = require('express').Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+router.use('/api', api);
