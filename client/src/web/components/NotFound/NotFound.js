d5ed52c2cf88fa73c280e91a75f88f019b7f953c
Gabrielle O'Brien <gobrien088@gmail.com>
2020-01-08 23:13:28 -0800

    Slight CSS changes

--- client/src/web/components/NotFound/NotFound.js
+++ client/src/web/components/NotFound/NotFound.js
@@ -6,7 +6,7 @@ export default function NotFound(){
     <div className="error">
       <h1>ERROR 404 - PAGE NOT FOUND</h1>
       <h2>OOPSIE WOOPSIE!</h2>
-      <img src="https://i.imgur.com/QYjhVX7.gif"/>
+      <img src="https://i.imgur.com/QYjhVX7.gif" alt="dancin"/>
       <p>
       <br/><br/>
         UwU We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquarters are working
