afe75e6c2fa0813c7d1b05c4fe3b9b7c68d41c36
SwampApe <rothvann2000@gmail.com>
2020-01-10 23:42:22 -0800

    Bot adds author and avatar url

--- blogBaseBot/index.js
+++ blogBaseBot/index.js
@@ -6,8 +6,8 @@ const knex = require('../database/database');
 function addPost(message) {
     knex('posts').insert({
         id: message.id,
-        author: message.author.username,
-        avatar: message.member.nickname,
+        author: message['member'].nickname,
+        avatar: message['author'].displayAvatarURL,
         date: message.createdAt.toISOString(),        
         content: message.content
     }).then( result => {console.log('Added message')}, result => {console.log(result)});
@@ -20,6 +20,16 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   for([id, guild] of client.guilds) {
       console.log(guild.name);
+      for([id, channel] of guild.channels) {
+            console.log(channel.name);
+            if(channel.type == 'text') {
+                channel.fetchMessages({limit: 50}).then(messages => { 
+                    for([id, message] of messages) {
+                        addPost(message);
+                    }
+                });
+            }
+      }
   }
 });
 
