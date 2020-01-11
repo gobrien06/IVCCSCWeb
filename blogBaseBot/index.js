2ec0b9883e1f0286c3863009643d6a22a7792782
SwampApe <rothvann2000@gmail.com>
2020-01-10 16:33:09 -0800

    Working bot

--- blogBaseBot/index.js
+++ blogBaseBot/index.js
@@ -1,4 +1,21 @@
-﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-; #Warn  ; Enable warnings to assist with detecting common errors.
-SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
-SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.EMMA);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  for([id, guild] of client.guilds) {
+      console.log(guild.name);
+  }
+});
+
+client.on('message', (message) => {
+    if(message.channel.name === 'annoucements') {
+        //log
+        
+    }
+});
+
+
+
