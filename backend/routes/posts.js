9d5671dd104b1ae060927f0a370f34a66a4f62fe
SwampApe <rothvann2000@gmail.com>
2020-01-11 00:35:58 -0800

    Bot uses username when nickname is not available and orders post by date when returning

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var posts = require('../models/posts')
 
-router.posts('/getPosts', function(req, res) {
+router.post('/getPosts', function(req, res) {
   posts.getPosts(res);
 });
 
