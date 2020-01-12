2c47d2311b7fcda0ca62e6ce26ea4c608ce09cf5
Gabrielle O'Brien <gobrien088@gmail.com>
2020-01-12 13:22:26 -0800

    mobile css adjustments

--- client/src/web/components/Header/FeaturedPost/FeaturedPost.js
+++ client/src/web/components/Header/FeaturedPost/FeaturedPost.js
@@ -9,8 +9,8 @@ export default class FeaturedPost extends Component{
     super(props);
     this.state={
       content:
-      "Lorem ipsum dolor sit amet, nec timeam docendi placerat ea, amet sententiae theophrastus ex ... Usu ad etiam clita nominati, pri putant iracundia ea. Te mei erat dicat aperiam, cum an euismod forensibus, nec an odio elit intellegebat. ... Et usu fierent detracto perfecto, ea odio volutpat splendide pri, solum forensibus est ei.",
-      icon:"https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Irvine_Valley_College_seal.svg/1200px-Irvine_Valley_College_seal.svg.png",
+      "        ",
+      icon:" ",
       author:"Author Name",
       date:"MM/DD/YY",
       success:false,
@@ -49,13 +49,13 @@ export default class FeaturedPost extends Component{
             {this.state.date}
           </CardTitle>
           <Row>
-            <Col md = "2" xs="4" sm="4" className = "side">
+            <Col md = "3" xs="4" sm="4" className = "side">
               <img className="icon" src={this.state.icon} alt="Icon" />
               <p className ="date">
               <br />
               {this.state.author}</p>
             </Col>
-            <Col md = "10" xs="8" sm="8">
+            <Col md = "9" xs="8" sm="8">
               <p className="previewtext">{this.state.content}</p>
               <ReadMore content={this.state.content} author={this.state.author} icon={this.state.icon} date={this.state.date}/>
            </Col>
