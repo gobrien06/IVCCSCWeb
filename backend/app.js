e84814d1ebf99a6a3442874d268c9965b6d83dbf
Ubuntu <rothvann2000@gmail.com>
2020-01-13 09:03:49 +0000

    Deployment changes

--- backend/app.js
+++ backend/app.js
@@ -3,18 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var routes = require('./routes');
+
 var cors = require('cors');
 
 var app = express();
 app.use(cors());
 
-
+var routes = require('./routes');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+console.log('asdf');
 
 app.use(routes);
 
