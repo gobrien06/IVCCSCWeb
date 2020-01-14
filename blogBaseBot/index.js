9c90d5e396129a05c2bd7de53d19d97dfd491f76
Gabrielle O'Brien <gobrien088@gmail.com>
2020-01-14 12:51:41 -0800

    rebase

---
+++ blogBaseBot/index.js
@@ -0,0 +1,21 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.login(process.env.EMMA);
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  for([id, guild] of client.guilds) {
+      console.log(guild.name);
+  }
+});
+
+client.on('message', (message) => {
+    if(message.channel.name === 'annoucements') {
+        //log
+        
+    }
+});
+
+
+
